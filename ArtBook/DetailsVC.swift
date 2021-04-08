89bc93ad581fa1cee949de8dd295c089e0ce1f24
amber <ambercatalbas@gmail.com>
2021-04-08 10:16:28 +0300

    first commit

--- ArtBook/DetailsVC.swift
+++ ArtBook/DetailsVC.swift
@@ -7,24 +7,125 @@
 //
 
 import UIKit
+import CoreData
 
-class DetailsVC: UIViewController {
-
+class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var yearText: UITextField!
+    @IBOutlet weak var artistText: UITextField!
+    @IBOutlet weak var saveButton: UIButton!
+    
+     var chosenPainting = ""
+    var chosenPaintingId : UUID?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        useDATA()
+   hideKeyboard()
+// hide keyboard recognizer
+//        let gestureRecegnizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+//        view.addGestureRecognizer(gestureRecegnizer)
+        
+    //select image recognizer
+        imageView.isUserInteractionEnabled = true
+        let imageTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+         imageView.addGestureRecognizer(imageTapRecognizer)
+    }
+    func useDATA() {
+        if chosenPainting != "" {
+               //core data
+               //saveButton.isEnabled = false
+               saveButton.isHidden = true
+               let appDelegate = UIApplication.shared.delegate as! AppDelegate
+               let context = appDelegate.persistentContainer.viewContext
+               
+               let fetchReguest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+               let idString = chosenPaintingId?.uuidString
+               
+               fetchReguest.predicate = NSPredicate(format: "id = %@", idString!)
+               fetchReguest.returnsObjectsAsFaults = false
+               
+               do {
+                   let results = try context.fetch(fetchReguest)
+                   if results.count > 0 {
+                       for result in results as! [NSManagedObject] {
+                           if let name = result.value(forKey: "name") as? String {
+                               nameText.text = name
+                           }
+                           if let artist = result.value(forKey: "artist") as? String {
+                               artistText.text = artist
+                           }
+                           if let year = result.value(forKey: "year") as? Int {
+                               yearText.text = String(year)
+                           }
+                           if let imageData = result.value(forKey: "image") as? Data {
+                               let image = UIImage(data: imageData)
+                               imageView.image = image
+                           }
+                       }
+                   }
+                   
+                   }
+                   catch {
+                   print("error")
+               }
+              
+               }
+               else {
+               saveButton.isHidden = false
+               saveButton.isEnabled = false
+                   nameText.text = ""
+                   yearText.text = ""
+                   artistText.text = ""
+                   
+               }
+        
+    }
+   // hide keyboard
+//    @objc func hideKeyboard () {
+//        view.endEditing(true)
+//    }
+    @objc func selectImage() {
+         let picker = UIImagePickerController()
+           picker.delegate = self
+           picker.sourceType = .photoLibrary                //select where/how
+           picker.allowsEditing = true                     // editing
+           present(picker, animated: true, completion: nil)
+       }
+       func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+           imageView.image = info[.originalImage] as? UIImage
+           saveButton.isEnabled = true
+           self.dismiss(animated: true, completion: nil)
+       }
+    @IBAction func buutonClicked(_ sender: Any) {
+     
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newPainting = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context)
+        
+        //Attributs
+        newPainting.setValue(nameText.text, forKey: "name")
+        newPainting.setValue(artistText.text, forKey: "artist")
+        if let year = Int(yearText.text!) {
+         newPainting.setValue(year, forKey: "year")
+        }
+        newPainting.setValue(UUID(), forKey: "id")
+        
+        let data = imageView.image?.jpegData(compressionQuality: 0.5) //SAVE %
+        newPainting.setValue(data, forKey: "image")
+        
+        do {
+            try context.save()
+            print("success")
+            }
+        catch {
+                print("error")
+        }
+        NotificationCenter.default.post(name: NSNotification.Name("newData"), object: nil)
+        self.navigationController?.popViewController(animated: true) //geri gelmeyi sağlıyor resmi kayd.sonra
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
